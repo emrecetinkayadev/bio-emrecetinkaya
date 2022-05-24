8b553d9a99c3bd83d211528aa931ca0b42a70b72
Emre Cetinkaya <emrecetinkayadev@gmail.com>
2022-05-24 09:07:44 +0300

    New Update and Button

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,10 +1,16 @@
 var myLink = document.getElementById('mylink');
 var divRes = document.getElementById('divRes');
 
-myLink.onclick(function(){
+myLink.addEventListener('click', function(){
+  console.log(divRes)
     divRes.animate({height: '300px', opacity: '0.4'}, "slow");
     divRes.animate({width: '300px', opacity: '0.8'}, "slow");
     divRes.animate({height: '100px', opacity: '0.4'}, "slow");
     divRes.animate({width: '100px', opacity: '0.8'}, "slow");
-    alert("Damn Son Where Did You Find This?")
   });
+
+  const _ = require('lodash');
+  const mottos = [
+    {id: 1, name: 'Wow Such Good Much Respect'},
+    {id: 2, name: 'Damn Son Where Did You Find This'}
+  ] 
