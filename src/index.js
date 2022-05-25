1bc76a291567178a3c5da751c260a6e97d53bd82
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-05-26 00:19:33 +0300

    Sass Webpack Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import "../sass/style.scss";
+/* import "../node_modules/bootstrap/js/bootstrap.min.js";
+import "../node_modules/jquery/dist/jquery.min.js"; */
 import { animate } from "./scripts/animate.js";
 import { alert } from "./scripts/alert.js";
+
  
